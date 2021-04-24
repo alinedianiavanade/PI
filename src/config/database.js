0aed24459a1bd711d40813b9e014a69d3bd72dd2
paulohlg92 <75283365+paulohlg92@users.noreply.github.com>
2021-04-24 17:37:23 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,11 @@ module.exports = {
 	dialect: 'mysql',
 	host: 'localhost',
 	username: 'root',
-  password: '',
+  	password: '',
 	database: 'pi_teste',
-  define: {
-			timestamps: true,    
-			underscored: true,  
-			underscoredAll: true,
+ 	define: {
+		timestamps: true,    
+		underscored: true,  
+		underscoredAll: true,
 	},
 };
