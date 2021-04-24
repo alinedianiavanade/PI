6e3db3989682341fb6498724a5caaa816f00c6b4
paulohlg92 <75283365+paulohlg92@users.noreply.github.com>
2021-04-24 15:36:47 -0300

    Update database.js

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,8 @@ module.exports = {
 	dialect: 'mysql',
 	host: 'localhost',
 	username: 'root',
-	password: 'easy3868',
-	database: 'pi',
+	password: '',
+	database: 'pi_teste',
 		define: {
 			timestamps: true,    
 			underscored: true,  
