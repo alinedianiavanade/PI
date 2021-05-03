17ffb363c8ffe9941ed63d33ddded1bb61903de8
Paulo Henrique <phguilherme92@gmail.com>
2021-05-03 15:41:51 -0300

    Ajuste Conflitos

--- src/config/database.js
+++ src/config/database.js
@@ -2,7 +2,7 @@ module.exports = {
 	dialect: 'mysql',
 	host: 'localhost',
 	username: 'root',
-  password: 'easy3868',
+  password: '',
 	database: 'pi_teste',
  	define: {
 		timestamps: true,    
