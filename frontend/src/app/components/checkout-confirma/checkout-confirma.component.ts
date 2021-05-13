0514eaef1ea76113bf9b679d654d80fc4d018701
Fernanda Loretto <fernanda.loretto3105@gmail.com>
2021-05-13 15:27:06 -0300

    Commit final MESMO

--- frontend/src/app/components/checkout-confirma/checkout-confirma.component.ts
+++ frontend/src/app/components/checkout-confirma/checkout-confirma.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PedidoService } from '../../services/pedidos.service';
+import { Carrinho } from '../../models/carrinho.model';
+import { CarrinhoService } from '../../services/carrinho.service';
 
 @Component({
   selector: 'app-checkout-confirma',
@@ -8,12 +10,25 @@ import { PedidoService } from '../../services/pedidos.service';
 })
 export class CheckoutConfirmaComponent implements OnInit {
 
-  constructor(public pedidoService: PedidoService) { }
+  constructor(public pedidoService: PedidoService,public carrinhoService: CarrinhoService,) { }
 
   ngOnInit(): void {
   }
   removePedidos(): void {
     this.pedidoService.deleteByCliente()
+      .subscribe(
+        (response) => {
+          this.removeCarrinho();
+          console.log(response);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+  
+  removeCarrinho(): void {
+    this.carrinhoService.delete()
       .subscribe(
         (response) => {
           console.log(response);
--- frontend/src/app/components/checkout/checkout.component.ts
+++ frontend/src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Carrinho } from '../../models/carrinho.model';
 import { CarrinhoService } from '../../services/carrinho.service';
+import { TokenStorageService } from '../../_services/token-storage.service';
+import { ClienteService } from '../../services/cliente.service';
+import { Cliente } from '../../models/cliente.model';
+import jwt_decode from 'jwt-decode';
+
 
 
 @Component({
@@ -11,40 +16,52 @@ import { CarrinhoService } from '../../services/carrinho.service';
 export class CheckoutComponent implements OnInit {
 
   carrinho: Carrinho = {
-    id_cliente: '1',
+    id_cliente: '',
     valor_total: 1,
-  }
+  };
+  currentCliente?: Cliente;
+  message = '';
+  currentToken: any;
+  atualizado = false;
+  decoded: any;
 
-  constructor(public carrinhoService: CarrinhoService) { }
+
+  constructor(public carrinhoService: CarrinhoService,
+    private token: TokenStorageService,private clienteService: ClienteService) { }
 
   ngOnInit(): void {
+    this.currentToken = this.token.getCliente();
+    var tokenDec = this.currentToken.token
+    this.decoded = jwt_decode(tokenDec);
+    this.atualizado = false;
+    this.message = '';
+    this.getClienteAtual();
+  }
+  getClienteAtual(): void {
+    this.clienteService.get()
+      .subscribe(
+        data => {
+          this.currentCliente = data;
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+        });
   }
   saveCarrinho(): void {
     const data = {
-      id_cliente: this.carrinho.id_cliente,
+      id_cliente: this.currentCliente?.id_cliente,
       valor_total: this.carrinho.valor_total
     };
     this.carrinhoService.create(data)
       .subscribe(
         response => {
           console.log(response)
-          this.removeCarrinho()
         },
         error => {
           console.log(error);
         }
       )
   };
-  removeCarrinho(): void {
-    this.carrinhoService.delete()
-      .subscribe(
-        (response) => {
-          console.log(response);
-        },
-        (error) => {
-          console.log(error);
-        }
-      );
-  }
 
 }
